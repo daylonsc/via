4e80e5b698d023c7b19060ed6de9c32af270f867
dayloncosta <dayloncosta@hotmail.com>
2019-11-04 21:41:41 -0300

    criacao do projeto

---
+++ src/main/java/com/teste/viavarejo/ViavarejoApplication.java
@@ -0,0 +1,13 @@
+package com.teste.viavarejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViavarejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViavarejoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/viavarejo/ViavarejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.viavarejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViavarejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
